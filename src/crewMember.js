8429d2ce6dc947fd3d8fb3278af39394122ac638
Susanna Kosonen <susannakosonen@Susannas-MacBook-Pro.local>
2018-07-23 17:00:42 -0400

    Done.

--- src/crewMember.js
+++ src/crewMember.js
@@ -1,1 +1,23 @@
-class CrewMember {}
+class CrewMember {
+    constructor(position, ship) {
+        this.position = position;
+        if (ship) {
+            this.currentShip = ship
+        } else {
+            this.currentShip = "Looking for a Rig"
+        }
+        
+    }
+    engageWarpDrive() {
+        return (typeof this.currentShip==="object" && this.position === "Pilot") ? "warp!" : "had no effect"
+    }
+
+    setsInvisibility() {
+        return (typeof this.currentShip==="object" && this.position === "Defender") ? this.currentShip.cloaked=true : "had no effect"
+    }
+
+    chargePhasers() {
+        return (typeof this.currentShip==="object" && this.position === "Gunner") ? "charge phasers" : "had no effect"
+    }
+}
+
