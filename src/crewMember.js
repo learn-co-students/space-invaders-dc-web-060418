bd078b4aa6ab51c542f79d37b7cf91cec192311d
jchamberlain909 <jchamberlain909@gmail.com>
2018-07-23 13:59:21 -0400

    Done.

--- src/crewMember.js
+++ src/crewMember.js
@@ -1,1 +1,26 @@
-class CrewMember {}
+class CrewMember {
+    constructor(position){
+        this.position = position
+        this.currentShip = 'Looking for a Rig'
+    }
+    engageWarpDrive(){
+        if (this.currentShip === 'Looking for a Rig') {
+            return 'had no effect'
+        }
+    }
+    setsInvisibility() {
+        if (this.currentShip === 'Looking for a Rig') {
+            return 'had no effect'
+        }
+        if (this.position === 'Defender') {
+            this.currentShip.cloaked = !this.currentShip.cloaked
+        }
+        
+    }
+    chargePhasers() {
+        if (this.currentShip === 'Looking for a Rig') {
+            return 'had no effect'
+        }
+    }
+    
+}
--- src/spaceship.js
+++ src/spaceship.js
@@ -1,1 +1,18 @@
-class Spaceship {}
+class Spaceship {
+    constructor(name, crew =[], phasers, shields){
+        this.name = name 
+        this.phasers = phasers
+        this.shields = shields
+        this.crew = crew || []
+        this.cloaked = false
+        this.warpDrive = 'disengaged'
+        if (crew.length === 0) {
+            this.docked =true
+        } else {
+           this.docked = false 
+        }
+        this.phasersCharge = 'uncharged'
+
+        crew.forEach((crewMember)=>crewMember.currentShip = this)
+    }
+}
