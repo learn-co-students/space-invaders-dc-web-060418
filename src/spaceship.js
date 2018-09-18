760bffdb5cf5a555f97f1df06fe3bef1641d2b64
j-shilling <shilling.jake@gmail.com>
2018-09-18 11:44:31 -0400

    Done.

--- src/spaceship.js
+++ src/spaceship.js
@@ -1,1 +1,17 @@
-class Spaceship {}
+class Spaceship {
+
+	constructor(name, crew, phasers, shields) {
+		this.name = name;
+		this.crew = crew;
+		this.phasers = phasers;
+		this.shields = shields;
+		this.cloaked = false;
+		this.warpDrive = "disengaged";
+		this.docked = this.crew.length === 0;
+		this.phasersCharge = "uncharged";
+
+		for (const member of crew) {
+			member.currentShip = this
+		}
+	}
+}
